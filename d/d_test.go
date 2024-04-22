be8c9972f41fd866e50720f6853f36157d8cb5b1
esm <emetsger@gmail.com>
2024-04-21 22:30:36 -0400

    initial commit.

---
+++ d/d_test.go
@@ -0,0 +1,51 @@
+package d
+
+import (
+	"context"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+	"log"
+	"testing"
+	"time"
+)
+
+func TestD(t *testing.T) {
+	//time.Sleep(5 * time.Second)
+	ctx := context.Background()
+
+	dbName := "users"
+	dbUser := "user"
+	dbPassword := "password"
+
+	postgresContainer, err := postgres.RunContainer(ctx,
+		testcontainers.WithImage("docker.io/postgres:16-alpine"),
+		//postgres.WithInitScripts(filepath.Join("testdata", "init-user-db.sh")),
+		//postgres.WithConfigFile(filepath.Join("testdata", "my-postgres.conf")),
+		postgres.WithDatabase(dbName),
+		postgres.WithUsername(dbUser),
+		postgres.WithPassword(dbPassword),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(5*time.Second)),
+		testcontainers.CustomizeRequest(testcontainers.GenericContainerRequest{
+			ContainerRequest: testcontainers.ContainerRequest{
+				Env: map[string]string{
+					"RYUK_CONNECTION_TIMEOUT":   "30s",
+					"RYUK_RECONNECTION_TIMEOUT": "30s",
+				},
+			},
+		}),
+	)
+	if err != nil {
+		log.Fatalf("failed to start container: %s", err)
+	}
+
+	// Clean up the container
+	defer func() {
+		if err := postgresContainer.Terminate(ctx); err != nil {
+			log.Fatalf("failed to terminate container: %s", err)
+		}
+	}()
+}
